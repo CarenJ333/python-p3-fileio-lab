996c2d833a45e324e90811c9bd235a6b54dfb443
Caren <carenjepchumba4@gmail.com>
2025-08-25 19:30:01 +0300

    finished lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+
+    with open(full_file_name, "w", encoding="utf-8") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+
+    with open(full_file_name, mode="a", encoding="utf-8") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_file_name = f"{file_name}.txt"
+
+    with open(full_file_name, mode="r", encoding="utf-8") as f:
+        return f.read()
